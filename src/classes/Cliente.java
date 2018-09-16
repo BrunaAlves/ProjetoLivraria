4ec967baee036db6c67c6ee1c27ea0cc4d939841
Bruna Alves <brunahalves94@gmail.com>
2018-09-16 11:08:12 -0300

    Modifica classe Cliente

--- src/classes/Cliente.java
+++ src/classes/Cliente.java
@@ -11,5 +11,68 @@ package classes;
  * @author alunoces
  */
 public class Cliente {
+    private String cpf;
+    private String nome;
+    private String estadocivil;
+    private String telefone;
+    private String email;
+
+    public Cliente(String cpf, String nome, String estadocivil, String telefone, String email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.estadocivil = estadocivil;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public Cliente() {
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEstadocivil() {
+        return estadocivil;
+    }
+
+    public void setEstadocivil(String estadocivil) {
+        this.estadocivil = estadocivil;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "cpf=" + cpf + ", nome=" + nome + ", estadocivil=" + estadocivil + ", telefone=" + telefone + ", email=" + email + '}';
+    }
+    
+    
     
 }
